bfeebf8b251dddbc4a86126c9a1194310b27ae13
supark0206 <supark0206@gmail.com>
2023-10-06 20:50:46 +0900

    Initial commit

---
+++ src/test/java/com/assignment/recurit/RecuritApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.recurit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecuritApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
